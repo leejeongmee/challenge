e1d6d4e2c843044b2a30ee136eaf162807d723f9
leejeongmee <alwjdal980413@naver.com>
2020-03-10 22:27:51 +0900

    event.js

---
+++ 200224 problem/js/event.js
@@ -0,0 +1,12 @@
+function focusevent(id) {
+  let focusev = document.getElementById(id);
+  // focusev.style.borderColor = 'green';
+}
+
+function blurevent(id) {
+  let blurev = document.getElementById(id);
+  // focusev.style.borderColor = '#cccccc';
+}
+
+focusevent(id);
+blurEffect(id);
